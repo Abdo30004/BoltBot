9d6f688ad483be9cbc7dcc3d4a29cb413db11786
baraa2004 <65704682+baraa2004@users.noreply.github.com>
2022-09-10 19:59:11 +0000

    uptime

--- commands/devs/stats.js
+++ commands/devs/stats.js
@@ -1,6 +1,7 @@
 //template file
 const { Message, Client } = require("whatsapp-web.js");
 const os = require("os");
+const process=require("process")
 const pms = require("pretty-ms");
 
 module.exports = {
@@ -22,8 +23,9 @@ module.exports = {
     const groups = chats.filter((c) => c.isGroup).length;
     const users = chats.filter((c) => !c.isGroup).length;
     const blocked = (await client.getBlockedContacts()).length;
-    const uptime = pms(os.uptime()*1000, { verbose: true });
-    const string = `*BoltBot Stats:*\n\n*Uptime:* ${uptime}\n\n*Groups:* ${groups}\n\n*Users:* ${users}\n\n*Blocked:* ${blocked}`;
+    const RdpUptime = pms(os.uptime()*1000, { verbose: true });
+    const uptime=pms(Math.floor(process.uptime())*1000, { verbose: true });
+    const string = `*BoltBot Stats:*\n\n*RDP Uptime:* ${RdpUptime}\n\n*Bot Uptime:* ${uptime}\n\n*Groups:* ${groups}\n\n*Users:* ${users}\n\n*Blocked:* ${blocked}`;
     await message.reply(string);
   },
 };
